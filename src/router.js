822c2edaed12f84c13c3352a964e863738ad10f7
Estela <zuning28@qq.com>
2022-05-08 16:18:19 +0800

    v2

--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Page1 from './components/Page1.vue'
 import Page2 from './components/Page2.vue'
 import Page3 from './components/Page3.vue'
 import Page4 from './components/Page4.vue'
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         {
             path: '/',
